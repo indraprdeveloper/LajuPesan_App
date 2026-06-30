7fe665e5c479c13987b36a195131a82ed776f0e6
IndraPrimaRamadani <indraprimar@gmail.com>
2026-07-01 03:40:08 +0700

    benerin daftar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'livewire/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
